6b8dec79cd27d18047aea063a21e1284d382093d
Tabouri <tabourirayane@gmail.com>
2026-05-12 11:45:46 +0100

    Liens actifs sidebar + recherche Audit Logs

--- src/Controller/HistoryController.php
+++ src/Controller/HistoryController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\HistoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -12,12 +13,26 @@ class HistoryController extends AbstractController
 {
     #[Route('/history', name: 'app_history')]
     #[IsGranted('ROLE_USER')]
-    public function index(HistoryRepository $historyRepository): Response
+    public function index(HistoryRepository $historyRepository, Request $request): Response
     {
-        $histories = $historyRepository->findBy([], ['date' => 'DESC']);
+        $search = $request->query->get('search');
+
+        if ($search) {
+            $histories = $historyRepository->createQueryBuilder('h')
+                ->leftJoin('h.user', 'u')
+                ->leftJoin('h.asset', 'a')
+                ->where('h.action LIKE :search OR u.nom LIKE :search OR u.prenom LIKE :search OR a.brand LIKE :search OR a.model LIKE :search')
+                ->setParameter('search', '%' . $search . '%')
+                ->orderBy('h.date', 'DESC')
+                ->getQuery()
+                ->getResult();
+        } else {
+            $histories = $historyRepository->findBy([], ['date' => 'DESC']);
+        }
 
         return $this->render('history/index.html.twig', [
             'histories' => $histories,
+            'search' => $search,
         ]);
     }
 }
